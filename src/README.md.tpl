81d3da9af97fda33a16f20300d96830d7f911c64
EverStarck <EverStarckOne@protonmail.com>
2021-06-16 00:29:40 -0500

    get lastest articles with node

---
+++ src/README.md.tpl
@@ -0,0 +1,36 @@
+### Hey, Ever here 👋
+
+---
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/51029456/122140420-d7c8ed00-ce10-11eb-8bdc-bf50df6070a2.png" width="100%" title="Intro Card" alt="Intro Card">
+</p>
+
+My name is Ever Alejandro and I'm from Mexico 🌮 
+
+I'm a Front-End Junior Developer... hehe 😅
+
+---
+## [Read my lastest articles in blog.EverStarck.com](https:/blog.everstarck.com)
+
+<style scoped>
+.articles {
+display: flex; gap:5px;
+}
+@media only screen and (max-width: 300px) {
+  .articles {
+    flex-direction: column;
+  }
+}
+</style>
+<section class="articles">
+  %{{latest_articles}}%
+</section>
+
+<div align="center">
+  <a href="https://blog.everstarck.com">
+    <img src="https://user-images.githubusercontent.com/51029456/122142335-e1ecea80-ce14-11eb-91b4-de43ce15b745.gif" width="150"/>
+    <br>
+    <strong>Visit my personal website </strong>
+  </a>
+</div>
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { promises as fs } from "fs";
+import Parser from "rss-parser";
+
+const parser = new Parser();
+
+const getLatestArticlesFromBlog = () => parser
+    .parseURL("https://blog.everstarck.com/index.xml")
+    .then((data) => data.items);
+
+
+(async () => {
+  const [template, articles] = await Promise.all([
+    fs.readFile("./src/README.md.tpl", { encoding: "utf-8" }),
+    getLatestArticlesFromBlog(),
+  ]);
+
+  // create latest articles markdown
+  const latestArticlesMarkdown = articles
+    .slice(0, 3)
+    .map(article => {
+      const {title, link} = article
+      let img = article.content.match('src="(.*)" alt')[1] || 'https://user-images.githubusercontent.com/51029456/122140420-d7c8ed00-ce10-11eb-8bdc-bf50df6070a2.png'
+
+      let articleHtml = `
+<div style="margin: 0 0 20px 0; width: 100%; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+  <a href='${link}' target='_blank'>
+    <img width='100%' src='${img}' alt='Featured image of ${title}' />
+  </a>
+  <br>
+  <i>${title}</i>
+</div>
+         `
+      return articleHtml
+    })
+    .join("\n");
+
+  // replace all placeholders with info
+  const newMarkdown = template.replace("%{{latest_articles}}%",latestArticlesMarkdown);
+
+  await fs.writeFile("README.md", newMarkdown);
+})();
